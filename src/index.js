cdd3bbde2eb51db584f67530836a754284de3d7c
Ross Kaffenberger <rosskaff@gmail.com>
2015-10-06 00:17:06 -0400

    Disable browser history for static site

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from 'components/Root';
 import configureStore from './stores';
-import createBrowserHistory from 'history/lib/createBrowserHistory';
+// import createBrowserHistory from 'history/lib/createBrowserHistory';
 
 import initialState from './stores/initial-state';
 import {resetAnimation} from './action-creators';
@@ -12,5 +12,6 @@ const store  = configureStore(initialState);
 
 store.dispatch(resetAnimation());
 
-const node = <Root routerHistory={createBrowserHistory()} store={store}/>;
+// const node = <Root routerHistory={createBrowserHistory()} store={store}/>;
+const node = <Root store={store}/>;
 ReactDOM.render(node, target);
